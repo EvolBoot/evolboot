63a91b68599d7f97af071a1f7a857b596902bed6
evoldevel <evolboot@proton.me>
2024-02-01 16:55:24 +0800

    feat: 批量保存更新

--- module/core/src/main/java/org/evolboot/core/data/BaseRepository.java
+++ module/core/src/main/java/org/evolboot/core/data/BaseRepository.java
@@ -1,11 +1,15 @@
 package org.evolboot.core.data;
 
+import java.util.List;
+
 /**
  * @author evol
  */
 public interface BaseRepository<T, ID> {
 
     void deleteAllByIdInBatch(Iterable<ID> ids);
+
+    <S extends T> List<S> saveAll(Iterable<S> entities);
 
 
 }
